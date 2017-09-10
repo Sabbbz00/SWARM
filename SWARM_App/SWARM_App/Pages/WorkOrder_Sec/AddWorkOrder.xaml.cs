63079000481ef83207303ff8f88b43abf9da9785
Sabbbz00 <kim96montes@gmail.com>
2017-09-10 10:20:08 -0400

    More Navigation

---
+++ SWARM_App/SWARM_App/Pages/WorkOrder_Sec/AddWorkOrder.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SWARM_App.Pages.WorkOrder_Sec
+{
+    public partial class AddWorkOrder : ContentPage
+    {
+        public AddWorkOrder()
+        {
+            InitializeComponent();
+        }
+		private void NextClicked(object sender, EventArgs e)
+		{
+			Navigation.PopAsync();
+		}
+    }
+}
